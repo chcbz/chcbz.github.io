bc96ddf41d301a478d47afa713346948d8906c67
chcbz <chcbz@sina.com>
2023-03-29 00:06:39 +0800

    增加搜索插件；样式默认简体中文；

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,19 +1,44 @@
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-
+  plugins: [
+    docsearchPlugin({
+      appId: 'JE82SHHB3C',
+      apiKey: '4b0d8533b0aaabb31bb5eecbbb4c315c',
+      indexName: 'chcbz',
+      locales: {
+        '/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+            },
+          },
+        },
+        '/en/': {
+          placeholder: 'Search Documentation',
+          translations: {
+            button: {
+              buttonText: 'Search Documentation',
+            },
+          },
+        },
+      },
+    }),
+  ],
   locales: {
     "/": {
-      lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
-    },
-    "/zh/": {
       lang: "zh-CN",
       title: "文档演示",
       description: "vuepress-theme-hope 的文档演示",
+    },
+    "/en/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
     },
   },
 
