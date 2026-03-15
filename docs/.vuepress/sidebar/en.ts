7c92669d5a490b5c7c9e1ed42c57c127a23d1565
chcbz <chcbz@sina.com>
2026-03-15 12:13:11 +0800

    fix: restore demo and article sections in sidebar configuration

--- docs/.vuepress/sidebar/en.ts
+++ docs/.vuepress/sidebar/en.ts
@@ -3,19 +3,18 @@ import { sidebar } from "vuepress-theme-hope";
 export const enSidebar = sidebar({
   "/en/": [
     "",
-    // {
-    //   icon: "discover",
-    //   text: "Demo",
-    //   prefix: "demo/",
-    //   link: "demo/",
-    //   children: "structure",
-    // },
+    {
+      icon: "discover",
+      text: "Demo",
+      prefix: "demo/",
+      link: "demo/",
+      children: "structure",
+    },
     {
       text: "video",
       icon: "actions",
       prefix: "video/",
       children: "structure",
     },
-    "slides",
   ],
 });
--- docs/.vuepress/sidebar/zh.ts
+++ docs/.vuepress/sidebar/zh.ts
@@ -3,19 +3,18 @@ import { sidebar } from "vuepress-theme-hope";
 export const zhSidebar = sidebar({
   "/": [
     "",
-    // {
-    //   icon: "discover",
-    //   text: "文章",
-    //   prefix: "article/",
-    //   link: "article/",
-    //   children: "structure",
-    // },
+    {
+      icon: "discover",
+      text: "文章",
+      prefix: "article/",
+      link: "article/",
+      children: "structure",
+    },
     {
       text: "视频",
       icon: "actions",
       prefix: "video/",
       children: "structure",
     },
-    "slides",
   ],
 });
